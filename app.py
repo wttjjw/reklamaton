f5fbe617ddaadc024657e64b3938677cb5825cf4
wttjjw <164053317+wttjjw@users.noreply.github.com>
2025-07-18 14:05:29 +0300

    TESTING2

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("СКИБОБ")
+st.caption("СКИБОБ")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
