80cc92845eab71ec9890fe49690569e0dbcdb213
ho1era <lera.jubair@gmail.com>
2025-07-19 13:37:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,24 +35,17 @@ def back_button(label="←", target=None, key_suffix=""):
         st.session_state.msgs = []
         st.rerun()
 
-    # Стилизация только этой кнопки
-    st.markdown(f"""
-        <style>
-        button[data-testid="button-{btn_key}"] {{
-            border-radius: 50%;
-            width: 40px;
-            height: 40px;
-            font-size: 20px;
-            font-weight: bold;
-            background-color: #fff;
-            border: 2px solid #ccc;
-            color: #444;
-            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-        }}
-        button[data-testid="button-{btn_key}"]:hover {{
-            background-color: #f0f0f0;
-            transform: scale(1.05);
-        }}
+   button[kind="primary"] {
+    background: linear-gradient(145deg, #00DC00, #00AA00);
+    color: white;
+    font-weight: 700;
+    border-radius: 20px;
+    border: 2px solid #fff;
+}
+button[kind="primary"]:hover {
+    background-color: #00C400;
+}
+
         </style>
     """, unsafe_allow_html=True)
 
